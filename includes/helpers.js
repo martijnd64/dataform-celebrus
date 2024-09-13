55cfc68196ed022764d67d179f7907998f6dd94d
Martijn Drijver <martijn.drijver@achmea.nl>
2024-09-13 12:43:09 +0000

    created all tables

--- includes/helpers.js
+++ includes/helpers.js
@@ -1,17 +1,3 @@
-/*
-SELECT
-    "${constants.XML_AVERO_ACHMEA}" AS config,
-    cb_functions.XML_TO_JSON(raw_data) as xml_string
-  FROM
-    ${ref(constants.XML_PREFIX + constants.XML_AVERO_ACHMEA)}
-  UNION ALL
-  SELECT
-    "${constants.XML_CB_KLANTDOMEIN}" AS config,
-    cb_functions.XML_TO_JSON(raw_data) as xml_string
-  FROM
-    ${ref(constants.XML_PREFIX + constants.XML_CB_KLANTDOMEIN)}
-*/
-
 const getXmlData = (
   name,
   includeUnion = true,
@@ -68,30 +54,5 @@ const getNestedNode = (
   END
     AS node${nodeField.charAt(0).toUpperCase() + nodeField.slice(1)}`;
 };
-
-/*
-  CASE
-    WHEN JSON_VALUE(xml_string, '$.processing.graph.nodeDefinitions.matchNode.match.constraints.valueconstraint.attributePathName') IS NOT NULL THEN ARRAY( SELECT JSON_VALUE(xml_string, '$.processing.graph.nodeDefinitions.matchNode.match.constraints.valueconstraint.attributePathName'))
-    WHEN JSON_VALUE(xml_string, '$.processing.graph.nodeDefinitions.matchNode.match.constraints.valueconstraint[0].attributePathName') IS NOT NULL THEN (
-  SELECT
-    ARRAY_AGG(JSON_VALUE(nodeArr, '$.attributePathName'))
-  FROM
-    UNNEST(JSON_QUERY_ARRAY(xml_string, '$.processing.graph.nodeDefinitions.matchNode.match.constraints.valueconstraint')) AS nodeArr)
-    WHEN JSON_VALUE(xml_string, '$.processing.graph.nodeDefinitions.matchNode[0].match.constraints.valueconstraint.attributePathName') IS NOT NULL THEN (
-  SELECT
-    ARRAY_AGG(IFNULL(JSON_VALUE(nodeArr, '$.match.constraints.valueconstraint.attributePathName'), ''))
-  FROM
-    UNNEST(JSON_QUERY_ARRAY(xml_string, '$.processing.graph.nodeDefinitions.matchNode')) AS nodeArr)
-    WHEN JSON_VALUE(xml_string, '$.processing.graph.nodeDefinitions.matchNode[0].match.constraints.valueconstraint[0].attributePathName') IS NOT NULL THEN (
-  SELECT
-    ARRAY_AGG(IFNULL(JSON_VALUE(nodeArr, '$.attributePathName'), ''))
-  FROM
-    UNNEST(
-      (SELECT
-        JSON_QUERY_ARRAY(JSON_QUERY(xml_string, '$.processing.graph.nodeDefinitions.matchNode'), '$[0].match.constraints.valueconstraint'))
-    ) AS nodeArr)
-  END
-    AS nodeMatchAttribute,
-*/
 
 module.exports = { getXmlData, getNode, getNestedNode };
